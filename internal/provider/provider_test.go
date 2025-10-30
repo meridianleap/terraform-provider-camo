809cd15ef310206a60b07bc9ad16be3da8069e02
John Kaltenbach <kaltenbach.john@gmail.com>
2025-10-30 17:45:11 +0800

    tests

---
+++ internal/provider/provider_test.go
@@ -0,0 +1,24 @@
+// Copyright (c) The Camo Terraform Provider Authors
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+)
+
+var (
+	// testAccProtoV6ProviderFactories are used to instantiate a provider during
+	// acceptance testing. The factory function will be invoked for every Terraform
+	// CLI command executed to create a provider server to which the CLI can
+	// reattach.
+	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+		"camo": providerserver.NewProtocol6WithError(New("test", func() uuid.UUID {
+			return testUUID
+		})()),
+	}
+)
