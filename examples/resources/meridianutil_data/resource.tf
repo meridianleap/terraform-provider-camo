6a34e12ae9b36bc364b56eefc078aa0728e8d246
John Kaltenbach <kaltenbach.john@gmail.com>
2025-10-30 17:42:57 +0800

    example for data resource

---
+++ examples/resources/meridianutil_data/resource.tf
@@ -0,0 +1,24 @@
+resource "camo_data" "test" {
+  # As in terraform_data, this input will be exported as a non-sensitive
+  # `output` attribute.
+  input = "foo"
+
+  # This input will be exported as a sensitive `sensitive_input` attribute.
+  # Changing it will NOT trigger a replacement of this resource, and therefore
+  # will NOT cause the provisioner block to execute.
+  input_sensitive = "sensitive_bar"
+
+  # As in terraform_data, modifying these triggers causes replacement.
+  triggers_replace = [
+    "change me to trigger replacement",
+  ]
+
+  # This is probably why this resource is useful to you. If you want to be able
+  # to store a sensitive value in the resource state, AND be able to change that
+  # value without triggering this provisioner function, storing that value in
+  # `triggers_replace` is not appropriate. Instead, store it in input_sensitive.
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo \"I only run when terraform_data.foo is destroyed.\""
+  }
+}
